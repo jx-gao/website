a6ef5aff612ce80d93f15e732a7e9a1614441e50
Jia-Xin Gao <jiaxinabecgao@gmail.com>
2023-06-01 21:46:35 +0200

    Added navbar, logo, icons

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Page = () => {
       <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
         Hello, I&apos;m a software engineer!
       </Box>
-      <Box display={{md:'flex'}}>
+      <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Jia-Xin Gao
