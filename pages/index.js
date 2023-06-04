5341b618f87a3bc0c38a2a8e01f45cf22b86cf88
Jia-Xin Gao <jiaxinabecgao@gmail.com>
2023-06-04 19:17:42 +0200

    Added transitions between pages

--- pages/index.js
+++ pages/index.js
@@ -6,79 +6,82 @@ import {
   useColorModeValue,
   Button
 } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 import Section from '../components/sections'
 import Paragraph from '../components/paragraph'
-import { ChevronRightIcon } from '@chakra-ui/icons'
-import NextLink from 'next/link'
-import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a software engineer!
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Jia-Xin Gao
-          </Heading>
-          <p>Web dev / DevOps / Cloud</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            disply="inline-block"
-            borderRadius="full"
-            src="/images/dp.jpg"
-            alt="dp"
-          />
+          Hello, I&apos;m a software engineer!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>Paragraph</Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My Portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Jia-Xin Gao
+            </Heading>
+            <p>Web dev / DevOps / Cloud</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              disply="inline-block"
+              borderRadius="full"
+              src="/images/dp.jpg"
+              alt="dp"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={(0.2)}>
-        <Heading as="h3" variant="section-title" >
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>2020-2022</BioYear>
-          Completed Bachelors in Computer Science
-        </BioSection>
-      </Section>
-      <Section delay={(0.3)}>
-        <Heading as="h3" variant="section-title" >
-          Credits
-        </Heading>
-        <p>This website is built based on <a href='https://www.craftz.dog/' target='_blank'>Takuya Matsuyama's website</a></p>
-        <p>3D model <a href='https://skfb.ly/oH8MY' target='_blank'>Pusheen vs Noodle</a> by porgy is licensed under <a href='http://creativecommons.org/licenses/by/4.0/' target='_blank'>Creative Commons Attribution</a></p>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>Paragraph</Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={(0.2)}>
+          <Heading as="h3" variant="section-title" >
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>2020-2022</BioYear>
+            Completed Bachelors in Computer Science
+          </BioSection>
+        </Section>
+        <Section delay={(0.3)}>
+          <Heading as="h3" variant="section-title" >
+            Credits
+          </Heading>
+          <p>This website is built based on <a href='https://www.craftz.dog/' target='_blank'>Takuya Matsuyama's website</a></p>
+          <p>3D model <a href='https://skfb.ly/oH8MY' target='_blank'>Pusheen vs Noodle</a> by porgy is licensed under <a href='http://creativecommons.org/licenses/by/4.0/' target='_blank'>Creative Commons Attribution</a></p>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,22 +1,26 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/sections'
 import { WordGridItem, WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
+
 import dp from '../public/images/dp.jpg'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1,1,2]} gap={6}>
-        <Section>
-          <WorkGridItem id="id" title="Title" thumbnail={dp}>
-            Placeholder
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem id="id" title="Title" thumbnail={dp}>
+              Placeholder
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
